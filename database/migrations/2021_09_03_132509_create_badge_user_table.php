234d8685bdba35b6a49bcd155d17698e00667b14
samsab1995 <samirsabiee@gmail.com>
2021-09-03 18:13:45 +0430

    create badge_user migration

---
+++ database/migrations/2021_09_03_132509_create_badge_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBadgeUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('badge_user', function (Blueprint $table) {
+            $table->bigInteger('badge_id');
+            $table->bigInteger('user_id');
+            $table->timestamps();
+            $table->primary(['badge_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('badge_user');
+    }
+}
