1ee6b9bd230eb4452ccfa56bebe4952e34daa869
samsab1995 <samirsabiee@gmail.com>
2021-09-06 01:32:26 +0430

    create row userStat when user registered

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param User $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        $user->userStat()->create();
+    }
+}
