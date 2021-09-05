66cad5461b9a07fdac2193360a9884209ecdc6c2
samsab1995 <samirsabiee@gmail.com>
2021-09-05 15:44:52 +0430

    control new and store

--- app/Http/Controllers/BadgeController.php
+++ app/Http/Controllers/BadgeController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreBadgeRequest;
+use App\Models\Badge;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class BadgeController extends Controller
 {
-    //
+
+    /**
+     * BadgeController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function new()
+    {
+        return view('badges.new');
+    }
+
+    public function store(StoreBadgeRequest $request): RedirectResponse
+    {
+        Badge::create($request->validated());
+        return back();
+    }
 }
