87ab9eca89029b0112f7a71069219e6392231bdc
samsab1995 <samirsabiee@gmail.com>
2021-09-05 15:47:26 +0430

    control middleware and and store

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreReplyRequest;
+use App\Models\Topic;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use function GuzzleHttp\Promise\all;
 
 class ReplyController extends Controller
 {
-    //
+
+    /**
+     * ReplyController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Topic $topic, StoreReplyRequest $request): RedirectResponse
+    {
+        $topic->replies()->create([
+                'user_id' => auth()->user()->id,
+            ] + $request->validated());
+
+        return back();
+
+    }
+
 }
