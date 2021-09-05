2667d47d9a830c67d6aaf10885f4833fc044d112
samsab1995 <samirsabiee@gmail.com>
2021-09-05 15:47:44 +0430

    add store route

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -1,31 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="d-flex flex-column justify-content-center align-items-center">
-        <div class="col-8 card p-0 mb-3">
-            <div class="card-header">
-                {{ $topic->title }}
-            </div>
-            <div class="card-body">
-                <article>
-                    <p>{{ $topic->text }}</p>
-                </article>
-            </div>
-            <div class="card-footer">{{ $topic->created_at->diffForHumans() }} By {{ $topic->user->name }}</div>
-        </div>
-        <div class="col-md-8 mb-3 p-0">
-            <div class="card">
+    <form method="post" action="{{ route('reply.store', $topic->id) }}">
+        @csrf
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <div class="col-8 card p-0 mb-3">
+                <div class="card-header">
+                    {{ $topic->title }}
+                </div>
                 <div class="card-body">
-                        Reply text
+                    <article>
+                        <p>{{ $topic->text }}</p>
+                    </article>
                 </div>
-                <div class="card-footer text-muted">
-                    3 min ago by samir
-                </div>
+                <div class="card-footer">{{ $topic->created_at->diffForHumans() }} By {{ $topic->user->name }}</div>
             </div>
+            @foreach($topic->replies as $reply)
+                <div class="col-md-8 mb-3 p-0">
+                    <div class="card">
+                        <div class="card-body">
+                            <p>{{ $reply->text }}</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            {{ $reply->created_at->diffForHumans() }} By {{ $reply->user_name }}
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            <div class="col-8 form-group p-0">
+                <textarea type="text" class="form-control w-100" aria-label="reply" id="text" name="text"
+                          rows="6"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Send</button>
         </div>
-        <div class="col-8 form-group p-0">
-            <textarea type="text" class="form-control w-100" aria-label="reply" id="text" name="text" rows="6"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Send</button>
-    </div>
+    </form>
 @endsection
