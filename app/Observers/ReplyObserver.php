da07446e5a29b802a65b88e12ba9e338baef32bc
samsab1995 <samirsabiee@gmail.com>
2021-09-06 01:29:15 +0430

    increment xp and reply count when reply created

---
+++ app/Observers/ReplyObserver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Reply;
+
+class ReplyObserver
+{
+    /**
+     * Handle the Reply "created" event.
+     *
+     * @param Reply $reply
+     * @return void
+     */
+    public function created(Reply $reply)
+    {
+        $reply->user->incrementXp(Reply::XP);
+        $reply->user->incrementReplyCount();
+    }
+
+}
