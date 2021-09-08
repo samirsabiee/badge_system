62c8a7c6671bdcc3b80bc9fd2b6050afde63ab57
samsab1995 <samirsabiee@gmail.com>
2021-09-08 15:40:24 +0430

    add xp topic and reply scope

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -10,4 +10,19 @@ class Badge extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'type', 'icon_url', 'required_number'];
+
+    public function scopeXp($query)
+    {
+        return $query->where('type', 0);
+    }
+
+    public function scopeTopic($query)
+    {
+        return $query->where('type', 1);
+    }
+
+    public function scopeReply($query)
+    {
+        return $query->where('type', 2);
+    }
 }
