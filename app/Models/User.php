e20d789a9b341ed27fd96fb6ffb0e9a1e80fbf2c
samsab1995 <samirsabiee@gmail.com>
2021-09-08 15:43:36 +0430

    add badges relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -70,5 +71,10 @@ class User extends Authenticatable
     {
         $this->userStat->reply_count++;
         $this->userStat->save();
+    }
+
+    public function badges(): BelongsToMany
+    {
+        return $this->belongsToMany(Badge::class)->withTimestamps();
     }
 }
